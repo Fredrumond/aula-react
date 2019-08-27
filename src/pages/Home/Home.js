00dc18634cf1329c478bc1be3b65f3e93d73eefb
Frederico Drumond <fredrumond@gmail.com>
2019-08-26 21:52:15 -0300

    Continuação das aulas

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,19 +6,35 @@ export class Home extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			ip:''
+			lista:[]
 		}
 	}
 
 	componentDidMount(){
 
-		fetch('https://api.ipify.org/?format=json')
-			.then((response)=>{
-				return response.json();
-			})
-			.then((json) => {
-				this.setState({ip:json.ip});
-			});
+		// fetch('http://jsonplaceholder.typicode.com/posts')
+		// 	.then(r => r.json())
+		// 	.then(json => {
+		// 		this.setState({lista:json})
+		// 	});
+
+		let dados = {
+			title:'Post novo',
+			body: 'post novo conteudo',
+			userId:'1'
+		}
+
+		fetch('http://jsonplaceholder.typicode.com/posts',{
+			method:'POST',
+			body:JSON.stringify(dados),
+			headers:{
+				"Content-type":"application/json;charset=UTF-8"
+			}
+		})
+		.then(r => r.json())
+		.then(json => {
+			console.log(json)
+		})
 
 	}
 
@@ -26,14 +42,19 @@ export class Home extends Component {
 		return (
 			<div>
 				<h3>HOME</h3>
-				<div>
-					{this.state.ip == '' && 
-						<i>Carregando...</i>
-					}
-					{this.state.ip != '' && 
-						<p>Seu IP é: {this.state.ip}</p>
-					}
-				</div>
+				{this.state.lista.length == 0 && 
+					<i>Carregando</i>
+				}
+				{this.state.lista.length > 0 && 
+					<ul>
+						{this.state.lista.map((item)=>{
+							return (
+								<li>{item.title}</li>
+								);
+						})}
+					</ul>
+				}
+				<br/>
 				<Link to="/sobre">Ir para sobre</Link>						
 			</div>
 			);
